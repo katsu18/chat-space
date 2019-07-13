62c2485c6171dbe39a0399562ee83705abb953fa
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-07-13 15:51:09 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,18 +6,6 @@ class GroupsController < ApplicationController
         @group = Group.new
         @group.users.push(current_user)
     end
-
-    def show_last_message
-      if (last_message = messages.last).present?
-        if last_message.content?
-        last_message.content
-        else
-          '画像が投稿されています'
-        end
-      else
-        'まだメッセージはありません。'
-      end
-    end
   
     def create
       @group = Group.new(group_params)
@@ -43,7 +31,6 @@ class GroupsController < ApplicationController
     end
   
     def set_group
-      binding.pry
       @group = Group.find(params[:id])
     end
 
