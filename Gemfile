dd573bb1578a9cd4e74d98a50819d22bd8b86ac9
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-07-17 16:36:57 +0900

    非同期通信実装

--- Gemfile
+++ Gemfile
@@ -57,5 +57,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
  gem 'pry-byebug'
  gem "better_errors"
  gem "binding_of_caller"
- gem "mini_magick"
- gem "carrierwave"
+  gem "mini_magick"
+  gem "carrierwave"
+ gem 'jquery-rails'
---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,41 @@
+$(function(){
+
+  
+  function messagePost(post){
+    
+    var html =  '<div class="upper-info"><p class="upper-info__user">'+(post.name)+'</p><p class="upper-info__date">'+(post.date)+'</p></div><p class="message__text">'+(post.content)+'</p><img src="'+(post.image.url)+'">'
+    return html;
+
+  }
+
+
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'JSON',
+      processData: false,
+      contentType: false
+    })
+    .done(function(post){
+      if(post.image.url == null){
+        post.image.url = ""
+      }
+      var html = messagePost(post)
+      $(".message").append(html)
+      $("#message_content").val("")
+      $("#message_image").val("")
+      $('.form__submit').prop('disabled', false);
+      $('.messages').animate({scrollTop: $('.message')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert('エラー')
+      $('.form__submit').prop('disabled', false);
+    })
+  })
+});
+
