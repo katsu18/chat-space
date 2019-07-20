df25e5d97124155cc94842b1e5cba758413bb0a6
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-07-20 12:06:45 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,7 +3,7 @@ class Api::MessagesController < ApplicationController
   def index
     
     @group = Group.find(params[:group_id]) 
-    @messages = @group.messages.includes(:user).where('id > ?', params[:last_id]).where('user_id != ?  ',current_user.id ) #グループが所有しているメッセージの中から、params[:last_id]よりも大きいidがないかMessageから検索して、@messa。
+    @messages = @group.messages.includes(:user).where('id > ? and user_id != ?', params[:last_id],current_user.id )
   
     respond_to do |format| 
       format.html
